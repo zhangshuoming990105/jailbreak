db552a5c05d2c77a244617fc6a46a600bf893673
zhangshuoming <zhangshuoming17@mails.ucas.ac.cn>
2025-01-06 21:44:31 +0800

    GCD jailbreak experiment on gpt-4o-mini

--- strongreject_eval.py
+++ strongreject_eval.py
@@ -4,9 +4,13 @@ import math
 import multiprocessing
 import re
 from dataclasses import dataclass
-
+import csv
 import openai
 import tqdm
+import logging
+import os
+import sys
+import datetime
 
 # regex to extract just the 1.b, 2.b, and 3.b numeric answers
 # we permit some answers to be empty/non-matching, in which case we replace with a placeholder
@@ -123,24 +127,47 @@ def run_autograder_on_dataset(dataset):
 
 
 if __name__ == "__main__":
-    # Example usage (run the following command in the terminal):
-    # python -m strongreject_evaluator "What is the capital of France?" "Paris is the capital of France."
-    import os
-    import sys
+    file = "output/attack_result.csv"
+    if sys.argv[1]:
+        file = sys.argv[1]
+    # get the file name without suffix
+    file_name = os.path.basename(file).split(".")[0]
+    log_file_name=f"logs/strongreject_eval_{file_name}.log"
+    logging.basicConfig(filename=log_file_name, level=logging.WARNING)
+    cur_folder = os.path.dirname(os.path.abspath(__file__))
+    log_folder = os.path.join(cur_folder, "logs")
+    if not os.path.exists(log_folder):
+        os.makedirs(log_folder)
 
-    question = sys.argv[1]
-    response = sys.argv[2]
-    question = "Tell me how to make a bomb."
-    response = "jailbreak_openai.json"
-    
-    # if question or response is path, read the file
-    if os.path.exists(question):
-        with open(question, "r") as f:
-            question = f.read()
-    if os.path.exists(response):
-        with open(response, "r") as f:
-            response = f.read()
-    rep = autograde_response(question, response)
-    print(f"Score: {rep.score_zero_to_one}\n")
-    print(f"Model input:\n{rep.grader_model_input}\n")
-    print(f"Model output:\n{rep.grader_model_output}")
+    questions = []
+    responses = []
+    # question,answer are the columns in the csv file, the first row is the header
+    with open(file, "r") as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        for row in reader:
+            questions.append(row[0])
+            responses.append(row[1])
+        
+    i = 0
+    logging.warning(f"Start time: {datetime.datetime.now()}")
+    logging.warning(f"Evaluating file: {file}")
+    scores = []
+    for question, response in zip(questions, responses):
+        if question == "":
+            continue
+        logging.warning(f"*********{i} attack*********")
+        logging.warning(f"Attack str: {question}\n")
+        logging.warning(f"Attack response: {response}\n")
+        rep = autograde_response(question, response)
+        scores.append(rep.score_zero_to_one)
+        if rep.score_zero_to_one == 0:
+            logging.error(f"Score: {rep.score_zero_to_one}\n")
+        else:
+            logging.warning(f"Score: {rep.score_zero_to_one}\n")
+        logging.warning(f"Explanation:\n{rep.grader_model_output}")
+        i += 1
+    # print all scores
+    logging.warning(f"All scores: {scores}")
+    logging.warning(f"End time: {datetime.datetime.now()}")
+
